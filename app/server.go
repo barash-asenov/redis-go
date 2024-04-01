f9705387a6176a868ce82db94f8ab31a98fc384a
Barash Asenov <klaibthe@gmail.com>
2024-04-01 11:46:27 +0200

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
-	// Uncomment this block to pass the first stage
-	// "net"
-	// "os"
 )
 
 func main() {
@@ -20,9 +18,40 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
+
+	log.Println("Will receive")
+
+	for {
+		content, err := readFromConnection(conn)
+		if err != nil {
+			log.Fatal("Failed to read from connection:", err)
+		}
+
+		log.Println("Received:", string(content))
+
+		writeContent := []byte("+PONG\r\n")
+
+		_, err = conn.Write(writeContent)
+		if err != nil {
+			log.Fatal("Failed to write to connection:", err)
+		}
+	}
+}
+
+func readFromConnection(conn net.Conn) ([]byte, error) {
+	buf := make([]byte, 4096)
+
+	n, err := conn.Read(buf)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to read from the connection: %w", err)
+	}
+
+	return buf[:n], nil
 }
